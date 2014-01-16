add9e5c0bcba42997c3ba334e4bf9437354ce5b8
HanbinKim <hottuc@naver.com>
2014-01-16 14:43:45 +0900

    Ex.5.3.8 / p182
    Ex.5.3.8 / p182

---
+++ Ex 5.3.8/ViewController.m
@@ -0,0 +1,68 @@
+//
+//  ViewController.m
+//  Ex 5.3.8
+//
+//  Created by SDT-1 on 2014. 1. 16..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#define MOVIE_URL @"http://movies.apple.com/media/kr/iphone/2011/tours/apple-iphone4s-feature_keynote-kr-20111110_r848-9cie.mov"
+
+@interface ViewController (){
+    MPMoviePlayerController *player;
+}
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleStateChanged:) name:@"MPMoviePlayerPlaybakcStateDidChangeNotification" object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleMediaTypeNoti:) name:@"MPMovieSourceTypeAvailableNotification" object:nil];
+    
+   // NSURL *url = [[NSBundle mainBundle]URLForResource:@"movie" withExtension:@"mp4"];
+    
+    // 스트리밍 영상재생용
+    NSURL *url = [NSURL URLWithString:MOVIE_URL];
+    
+    NSAssert((nil!=url), @"url is nil");
+    
+    player = [[MPMoviePlayerController alloc] initWithContentURL:url];
+    [player prepareToPlay];
+    
+    [player.view setFrame:self.view.bounds];
+    
+    [self.view addSubview:player.view];
+    
+}
+
+- (void)handleMediaTypeNoti:(NSNotification *)noti
+{
+    MPMoviePlayerController *p = noti.object;
+    
+    NSLog(@"Media Source Type : %d",p.movieSourceType);
+}
+
+- (void)handleStateChanged:(NSNotification *)noti
+{
+    MPMoviePlayerController *p = noti.object;
+    NSLog(@"Playback state : %d",p.playbackState);
+
+}
+
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
